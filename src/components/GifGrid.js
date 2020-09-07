68b42d25fdb2261afc1d68233476ecf5d56216f6
Sion Trade <siontrade@Sions-MacBook-Pro.local>
2020-09-07 18:26:52 -0500

    update 07-09-20 18:26

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
 import { GifGridItem } from './GifGridItem.js'
-import { getGift } from '../helpers/getGifs.js'
 
 export const GifGrid = ({ category }) => {
+    const {data:images, loading} = useFetchGifs(category)
 
-    const [images, setImages] = useState([])
-
-    useEffect(() => {
-        getGift(category)
-            .then(imgs => setImages( imgs))
-    }, [category])
+    // useEffect(() => {
+    //     getGift(category)
+    //         .then(imgs => setImages( imgs))
+    // }, [category])
 
     return (
         <>
             <h3>{ category }</h3>
+            {loading && <p>Loading...</p>}
             <div className='card-grid'>
                 {
                     images.map(img => (
