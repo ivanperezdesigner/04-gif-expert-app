07a521cb05b6afd1771e307f4c5359e5e56bff46
Sion Trade <siontrade@Sions-MacBook-Pro.local>
2020-09-07 18:47:38 -0500

    added docs

---
+++ docs/precache-manifest.6fd985c947ee29eeff9d052c868b58ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ceee211ccdb373f551806a8139da56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "393cec75fa2e27fa3a4d",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "b063102ac09cce9517a9",
+    "url": "/static/css/main.09fe34de.chunk.css"
+  },
+  {
+    "revision": "393cec75fa2e27fa3a4d",
+    "url": "/static/js/2.9fff9853.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9fff9853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b063102ac09cce9517a9",
+    "url": "/static/js/main.d94a7094.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
