25b9a577502b7056e4a780e3746af0d33c706a12
Ivan Perez <joseivanperezdiaz1@gmail.com>
2020-09-17 16:28:54 -0500

    updt tests 17-09-20 16:28

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -4,9 +4,16 @@ import React from 'react'
 
 describe('Pruebas en el componente', () => {
     const setCategories = () => {}
+    const wrapper = shallow(<AddCategory setCategories={ setCategories } />)
+    
     test('Debe mostrarse correctamente', () => {
-        const wrapper = shallow(<AddCategory setCategories={ setCategories } />)
         expect(wrapper).toMatchSnapshot()
     })
-    
+
+    test('evaluar los cambios en la caja de texto', () => {
+        const input = wrapper.find('input')
+        const value = 'Hola mundo'
+        input.simulate('change', {target: {value}})
+        expect (wrapper.find('p').text().trim()).toBe(value)
+    })
 })
