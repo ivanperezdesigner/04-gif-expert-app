63b9dfc96b12843ebecfc2ce6feff486842bbad0
Ivan Perez <joseivanperezdiaz1@gmail.com>
2020-09-18 11:40:54 -0500

    updt tests 18-09-20 11:40

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -1,10 +1,16 @@
 import { shallow } from 'enzyme'
 import { AddCategory } from '../../components/AddCategory'
 import React from 'react'
+import '@testing-library/jest-dom'
 
 describe('Pruebas en el componente', () => {
-    const setCategories = () => {}
-    const wrapper = shallow(<AddCategory setCategories={ setCategories } />)
+    const setCategories = jest.fn()
+    let wrapper = shallow(<AddCategory setCategories={ setCategories } />)
+
+    beforeEach(() => {
+        jest.clearAllMocks()
+        wrapper = shallow(<AddCategory setCategories={ setCategories } />)
+    })
     
     test('Debe mostrarse correctamente', () => {
         expect(wrapper).toMatchSnapshot()
@@ -15,5 +21,28 @@ describe('Pruebas en el componente', () => {
         const value = 'Hola mundo'
         input.simulate('change', {target: {value}})
         expect (wrapper.find('p').text().trim()).toBe(value)
+    })
+
+    test('No debe postear la información con submit', () => {
+        wrapper.find('form').simulate('submit', {preventDefault(){}})
+        expect(setCategories).not.toHaveBeenCalled()
+    })
+    
+    test('debe llamar el setCategories y limpiar la caja de texto', () => {
+        const value = 'Hola mundo'
+
+        // 1. Simular el input change
+        wrapper.find('input').simulate('change', {target:{value}})
+
+        // 2. Simular el Submit
+        wrapper.find('form').simulate('submit', {preventDefault(){}})
+
+        // 3. setCategories se debe haber llamado
+        expect(setCategories).toHaveBeenCalled()
+        expect(setCategories).toHaveBeenCalledTimes(1)
+        expect(setCategories).toHaveBeenCalledWith(expect.any(Function))
+
+        // 4. El valor del input debe estar vacío
+        expect(wrapper.find('input').prop('value')).toBe('')
     })
 })
